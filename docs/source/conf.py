10d3ed58cc3a3f6076febfbf31fb56128c953f5e
RedstoneHair <46073266+RedstoneHair@users.noreply.github.com>
2020-10-24 22:15:46 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,4 +14,4 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
 html_static_path = ['_static']
-html_theme = 'classic'
+html_theme = 'groundwork'
